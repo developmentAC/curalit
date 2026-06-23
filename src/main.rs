08e84eb1c867759faf1ebef0e7f0caa8cc4143df
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2026-06-22 21:47:38 -0400

    update version

--- src/main.rs
+++ src/main.rs
@@ -1079,7 +1079,7 @@ fn print_banner() {
     );
     println!(
         "{}",
-        "║                     CuraLit v0.3.3                        ║"
+        "║                     CuraLit v0.4.0                        ║"
             .cyan()
             .bold()
     );
