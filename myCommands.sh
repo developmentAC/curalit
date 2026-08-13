ec6735b5d094041f3fd57f3ffe0d3938f2aa6750
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2026-08-13 00:55:37 -0500

    update uv pyproject config

--- myCommands.sh
+++ myCommands.sh
@@ -1,5 +1,5 @@
-cargo clean
-cargo build --release
+# cargo clean
+# cargo build --release
 ./target/release/curalit search -k "study" -k "alpha" -k "protein" -d ./data --logic or -o results
 ./target/release/curalit stats -c 0_out/results_*.csv
 uv run 0_out/results_*_visualize.py
